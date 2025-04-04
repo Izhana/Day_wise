aa4b08662898bb3c54d9862564f15add805632af
Izhana <induchowdhury16972@gmail.com>
2025-04-04 10:21:52 -0700

    Delete Day 1/subtraction.c

--- Day 1/subtraction.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-  int a = 50, b = 20;
-  int c = a - b;
-  scanf("%d", &a);
-  printf( "The value of the subtraction is\n");
-  printf("%d %d %d\n", a, b, c);
-  return 0;
-}
