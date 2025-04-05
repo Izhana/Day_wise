dccae7503671c34efc8d9efa48041a36b3ba6ee0
Izhana <induchowdhury16972@gmail.com>
2025-04-05 12:25:28 +0530

    Update count_the num_of _digits.c

--- Day7/count_the num_of _digits.c
+++ Day7/count_the num_of _digits.c
@@ -4,13 +4,19 @@ int main()
     int number, sum = 0;
     printf("Enter an integer: ");
     scanf("%d", &number);
-
-   	 while (number>0 && n)!=
-		 {
+    if (number == 0) 
+    {
+        sum = 1;
+    } 
+    else 
+    {
+        while (number > 0) 
+	{
             sum++;
-            number = number/10; 
+            number = number / 10; 
         }
     }
+
     printf("The number of digits is: %d\n", sum);
 
     return 0;
